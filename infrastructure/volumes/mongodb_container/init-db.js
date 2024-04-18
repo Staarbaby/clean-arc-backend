a2bec172e2edb600aebb52f5f53210daa57390fd
Dany <dany.315920@yandex.ru>
2024-04-18 14:04:05 +0300

    Author-Domain Добавляем сущность и исключения

--- infrastructure/volumes/mongodb_container/init-db.js
+++ infrastructure/volumes/mongodb_container/init-db.js
@@ -1,5 +1,5 @@
 db = db.getSiblingDB('ftl-auth-service');
 db.createCollection('firstCollection');
 
-db = db.getSiblingDB('clean-acr-example');
+db = db.getSiblingDB('clean-arc-example');
 db.createCollection('firstCollection');
