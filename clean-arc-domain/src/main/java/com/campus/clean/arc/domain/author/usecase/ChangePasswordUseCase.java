b1eac49451418440077c4095d9d03a738bfe84a3
Dany <dany.315920@yandex.ru>
2024-04-22 19:02:15 +0300

    Author-Domain Смена пароля и работа с auth-lib

---
+++ clean-arc-domain/src/main/java/com/campus/clean/arc/domain/author/usecase/ChangePasswordUseCase.java
@@ -0,0 +1,46 @@
+package com.campus.clean.arc.domain.author.usecase;
+
+import com.campus.clean.arc.domain.author.entity.AuthorEntity;
+import com.campus.clean.arc.domain.author.exceptions.AuthorNotFoundException;
+import com.campus.clean.arc.domain.author.port.AuthorRepository;
+import com.rcore.domain.commons.usecase.UseCase;
+import com.rcore.domain.commons.usecase.model.SingleOutput;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+import ru.foodtechlab.lib.auth.integration.core.credential.CredentialServiceFacade;
+import ru.foodtechlab.lib.auth.service.facade.credential.dto.requests.ChangeCredentialPasswordRequest;
+
+import javax.validation.constraints.NotBlank;
+import java.time.Instant;
+
+@RequiredArgsConstructor
+public class ChangePasswordUseCase extends UseCase<ChangePasswordUseCase.InputValues, SingleOutput<Boolean>> {
+
+    private AuthorRepository authorRepository;
+    private CredentialServiceFacade credentialServiceFacade;
+
+    @Override
+    public SingleOutput<Boolean> execute(InputValues inputValues) {
+        AuthorEntity authorEntity = authorRepository.findById(inputValues.id).orElseThrow(AuthorNotFoundException::new);
+
+        credentialServiceFacade.changePassword(
+                authorEntity.getCredentialId(),
+                ChangeCredentialPasswordRequest.of(inputValues.password)
+        );
+
+        authorEntity.setUpdatedAt(Instant.now());
+        authorRepository.save(authorEntity);
+        return SingleOutput.of(true);
+    }
+
+    @AllArgsConstructor(staticName = "of")
+    @RequiredArgsConstructor(staticName = "empty")
+    @Builder
+    public static class InputValues implements UseCase.InputValues{
+        @NotBlank
+        private String id;
+        @NotBlank
+        private String password;
+    }
+}
